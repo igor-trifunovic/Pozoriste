b9b178274d25423dd81661bf0826fc64a8a5de84
Igor <igor.radovanov@outlook.com>
2018-08-20 11:36:37 +0200

    Initial

---
+++ PPP_Pozoriste/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PPP_Pozoriste
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
